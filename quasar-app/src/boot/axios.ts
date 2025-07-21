46d1036b0c902459ed6773821c4e7f853e8ca25b
loic <loicbachellerie@me.com>
2025-07-21 01:40:18 -0700

    Update environment variable for API URL and modify Axios base URL for dynamic configuration

--- quasar-app/src/boot/axios.ts
+++ quasar-app/src/boot/axios.ts
@@ -14,7 +14,7 @@ declare module 'vue' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: import.meta.env.API_URL || 'http://localhost:8000/api' });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'https://todo-laravel-main-5foawg.laravel.cloud/' });
 
 export default defineBoot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
